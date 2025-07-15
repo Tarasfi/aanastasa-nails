3af8832f8a0eee2af034cacfff2aed52055e2e77
Tarasfi <tarasfirko21@gmail.com>
2025-07-15 17:31:17 +0200

    Carousel add

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,5 @@
+
+// Navabar on the phone
 var navLinks = document.getElementById("navLinks");
 
 function openMenu() {
@@ -7,3 +9,19 @@ function openMenu() {
 function closeMenu() {
   navLinks.style.right = "-200px";
 }
+
+
+// Carousel slider animation
+const slider = document.querySelector(".slider");
+const dots = document.querySelectorAll(".slider-nav a");
+const slides = slider.querySelectorAll("img");
+
+slider.addEventListener("scroll", () => {
+  const scrollLeft = slider.scrollLeft;
+  const slideWidth = slider.clientWidth;
+
+  const index = Math.round(scrollLeft / slideWidth);
+
+  dots.forEach((dot) => dot.classList.remove("active"));
+  if (dots[index]) dots[index].classList.add("active");
+});
