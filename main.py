c25c749dff885de1abd334541ff9ce955b3fe4e7
Tarasfi <tarasfirko21@gmail.com>
2025-07-16 20:24:25 +0200

    Json Price add, HTML Refactor and Jinja made

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    with open ('data/price.json', encoding='utf-8') as p:
+        prices = json.load(p)
+        print(prices)
+    return render_template("index.html", prices=prices)
 
 
 if __name__ == '__main__':
--- static/js/script.js
+++ static/js/script.js
@@ -10,18 +10,6 @@ function closeMenu() {
   navLinks.style.right = "-200px";
 }
 
-
-// Carousel slider animation
-const slider = document.querySelector(".slider");
-const dots = document.querySelectorAll(".slider-nav a");
-const slides = slider.querySelectorAll("img");
-
-slider.addEventListener("scroll", () => {
-  const scrollLeft = slider.scrollLeft;
-  const slideWidth = slider.clientWidth;
-
-  const index = Math.round(scrollLeft / slideWidth);
-
-  dots.forEach((dot) => dot.classList.remove("active"));
-  if (dots[index]) dots[index].classList.add("active");
+document.getElementById('menu-btn').addEventListener('click', function() {
+  document.querySelector('.navbar').classList.toggle('open');
 });
