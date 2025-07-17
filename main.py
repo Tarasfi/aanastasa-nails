fe00d17c481c09e3847be72e8003e687431f481a
Tarasfi <tarasfirko21@gmail.com>
2025-07-17 19:27:42 +0200

    Directories modified

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+
+    return render_template("main/index.html")
+
+
+@app.route("/book_nails")
+def book_nails():
     with open ('data/price.json', encoding='utf-8') as p:
         prices = json.load(p)
-        print(prices)
-    return render_template("index.html", prices=prices)
 
+    return render_template("booking/booking_page.html", prices=prices)
 
 if __name__ == '__main__':
     app.run(debug=True)
