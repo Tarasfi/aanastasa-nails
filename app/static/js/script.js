f9fcb1ea407811fad80cb9d49d8e57bacba93aad
Tarasfi <tarasfirko21@gmail.com>
2025-08-09 17:03:32 +0200

    Mobile Navbar horizontal scroll fix

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -13,3 +13,17 @@ function closeMenu() {
 document.getElementById('menu-btn').addEventListener('click', function() {
   document.querySelector('.navbar').classList.toggle('open');
 });
+
+window.addEventListener('load', function() {
+  navLinks.style.right = "-200px";  
+  document.querySelector('.navbar').classList.remove('open');
+});
+
+function openMenu() {
+  document.querySelector('.navbar').classList.add('open');
+}
+
+function closeMenu() {
+  document.querySelector('.navbar').classList.remove('open');
+}
+
